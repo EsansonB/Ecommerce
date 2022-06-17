89a0ece6c318f356c0aec3036d22cb8137f7f396
EsansonB <91915041+EsansonB@users.noreply.github.com>
2022-06-17 19:23:44 -0300

    creacion de la estructura del proyecto y las clases model

---
+++ src/test/java/projectJava/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package projectJava.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
