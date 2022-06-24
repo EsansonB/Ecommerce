11b914c7874085198a37ad766ef4615682f41e9b
EsansonB <91915041+EsansonB@users.noreply.github.com>
2022-06-24 19:36:33 -0300

    funcionalidad mostrar productos para ADMIN

--- src/main/java/projectJava/ecommerce/service/ProductoServiceImplementacion.java
+++ src/main/java/projectJava/ecommerce/service/ProductoServiceImplementacion.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import projectJava.ecommerce.model.Producto;
 import projectJava.ecommerce.repository.ProductoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -34,5 +35,10 @@ public class ProductoServiceImplementacion implements ProductoService{
     public void delete(Integer id) {
         productoRepository.deleteById(id);
 
+    }
+
+    @Override
+    public List<Producto> findAll() {
+        return productoRepository.findAll();
     }
 }
