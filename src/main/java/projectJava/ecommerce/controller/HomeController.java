9a2e93afda09483b7b911304979507b5178f37c1
EsansonB <91915041+EsansonB@users.noreply.github.com>
2022-06-26 18:57:21 -0300

    validación doble producto y funcionalidad para ir al carrito carrito

--- src/main/java/projectJava/ecommerce/controller/HomeController.java
+++ src/main/java/projectJava/ecommerce/controller/HomeController.java
@@ -64,6 +64,16 @@ public class HomeController {
         detalleOrden.setNombre(producto.getNombre());
         detalleOrden.setTotal(producto.getPrecio()*cantidad);
         detalleOrden.setProducto(producto);
+        
+        //validar que el producto no se agregue 2 veces
+        Integer idProducto = producto.getId();
+        boolean ingresado= detalles.stream().anyMatch(p -> p.getProducto().getId()==idProducto);
+
+        if(!ingresado) {
+            detalles.add(detalleOrden);
+        }
+
+
 
         detalles.add(detalleOrden);
 
@@ -103,6 +113,15 @@ public class HomeController {
         model.addAttribute("orden", orden);
 
         return "usuario/carrito";
+
+    }
+
+    @GetMapping("/getCart")
+    public String getCart(Model model) {
+
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+        return "/usuario/carrito";
 
     }
 
