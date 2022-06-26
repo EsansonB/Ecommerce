8e8fcc93edb032a0614f9511c8563f89428da331
EsansonB <91915041+EsansonB@users.noreply.github.com>
2022-06-26 16:58:23 -0300

    mostrar toda la info del producto para comprar

--- src/main/java/projectJava/ecommerce/controller/HomeController.java
+++ src/main/java/projectJava/ecommerce/controller/HomeController.java
@@ -8,7 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import projectJava.ecommerce.model.Producto;
 import projectJava.ecommerce.service.ProductoService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -27,8 +30,13 @@ public class HomeController {
     }
 
     @GetMapping("productohome/{id}")
-    public String productoHome(@PathVariable Integer id) {
+    public String productoHome(@PathVariable Integer id, Model model) {
         log.info("Id producto enviado como parámetro {}", id);
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional = productoService.get(id);
+        producto = productoOptional.get();
+
+        model.addAttribute("producto", producto);
         return "usuario/productohome";
     }
 
