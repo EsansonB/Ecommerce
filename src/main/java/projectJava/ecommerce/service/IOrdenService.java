782f78364d9b1b39eab375b2ad6597af8ba97b8a
EsansonB <91915041+EsansonB@users.noreply.github.com>
2022-06-27 18:25:50 -0300

    para generar el numero de orden

--- src/main/java/projectJava/ecommerce/service/IOrdenService.java
+++ src/main/java/projectJava/ecommerce/service/IOrdenService.java
@@ -3,7 +3,10 @@ package projectJava.ecommerce.service;
 import org.springframework.stereotype.Service;
 import projectJava.ecommerce.model.Orden;
 
+import java.util.List;
+
 @Service
 public interface IOrdenService {
+    List<Orden> findAll();
     Orden save (Orden orden);
 }
